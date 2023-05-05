197a19bf8091271921f0cdc297716f33039f7de0
Niek van Duivenboden <niek.van.duivenboden@hotmail.com>
2023-05-05 12:52:08 +0200

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -1,7 +1,23 @@
 def decoreer(tekst=""):
+    tekst="header"
     lengte = len(tekst) + 4
     print()
     print(lengte * "*")
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+    #bedrag: het totaalbedrag van alle fooi
+    #personen: het aantal personen waarover het bedrag verdeeld moet worden
+def fooi_pp(bedrag, personen):
+      bedrag_pp = bedrag/personen
+      return f"het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):
+     uit=[]
+     uit.append(tekst)
+     uit.append(len(tekst) * "=")
+     return uit
+
+def som(inkomsten):
+    return(sum(inkomsten.values()))
---
+++ presentatie.py
@@ -0,0 +1,10 @@
+totaal = 0
+
+def presenteer(mijn_dict, totaal):
+   uitvoer = []
+   for x,y in mijn_dict.items():
+        print(x,y)
+   uitvoer = print(26 * "=")
+   totaal = sum(mijn_dict.values())
+   uitvoer = totaal
+   return uitvoer
